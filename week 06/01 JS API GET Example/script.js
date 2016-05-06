2e46f57ffc1a68cf346693835f0632ee263cae4d
knsy <ksy@yakovenko.net>
2016-05-05 17:53:17 -0700

    Clean Up
    Cleaned up the code and added annotations.

--- week 06/01 JS API GET Example/script.js
+++ week 06/01 JS API GET Example/script.js
@@ -17,26 +17,30 @@ var apiResp;
 // Creating the headers and data cells and appending them to rows
 // that we just created
 for(var i = 0; i < 3; i++){
+	//build up the URL
 	var url = "http://api.openweathermap.org/data/2.5/weather?zip="
 	url += zipcodes[i];
-	url += ",us";
-	url += "&units=metric";
+	url += ",us";  //In case we need to change the country
+	url += "&units=metric"; //In case we need to change to Imperial. Default is Kelvin.
 	url += "&appid="; 
 	url += apiKey;
 	
+	//GET the data back based on the URL we built up.
 	apiReq.open("GET",url, false);
 	apiReq.send(null);
 	apiResp = (JSON.parse(apiReq.responseText));
 	
-	
+	//FIll in the table header with the name of the city.
 	var newTableHeader = document.createElement("th");
 	newTableHeader.textContent = apiResp.name;
 	tableRow1.appendChild(newTableHeader);
 	
+	//fill in the second row with the zipcodes we put in.
 	var newTableData = document.createElement("td");
 	newTableData.textContent = zipcodes[i];
 	tableRow2.appendChild(newTableData);
 	
+	//fill in the third row with the actual temperature data.
 	var newTableData = document.createElement("td");
 	newTableData.textContent = apiResp.main.temp;
 	tableRow3.appendChild(newTableData);
@@ -51,12 +55,15 @@ newTable.appendChild(tableRow3);
 document.getElementById("tableWrapper").appendChild(newTable);
 
 //--------------------------Here we will process buttons--------
+// here we try to get the zipcode or a city name form the user and use that data to get the current temperature.
 
+
+//search by zipcode
 document.getElementById('zipSubmit').addEventListener('click',function(event){
 	var url = "http://api.openweathermap.org/data/2.5/weather?zip="
 	url += document.getElementById('weatherZip').value;
-	url += ",us";
-	url += "&units=metric";
+	url += ",us"; 	//In case we need to change the country
+	url += "&units=metric"; 	 //In case we need to change to Imperial. Default is Kelvin.
 	url += "&appid="; 
 	url += apiKey;
 	
@@ -65,15 +72,19 @@ document.getElementById('zipSubmit').addEventListener('click',function(event){
 	
 	apiResp = JSON.parse(apiReq.responseText);
 	
+	//display the parsed temperature for the zipcode we looked for
 	document.getElementById('inputMethod').textContent = document.getElementById('weatherZip').value;
 	document.getElementById('temperature').textContent = apiResp.main.temp + " degrees C";
 	
+	//make sure that the button doesnt refresh the page
 	event.preventDefault();
 });
+
+//search by city name
 document.getElementById('citySubmit').addEventListener('click', function(event){
 	var url = "http://api.openweathermap.org/data/2.5/weather?zip="
 	url += document.getElementById('weatherCity').value;
-	url += "&units=metric";
+	url += "&units=metric";	 //In case we need to change to Imperial. Default is Kelvin.
 	url += "&appid="; 
 	url += apiKey;
 	
@@ -82,9 +93,11 @@ document.getElementById('citySubmit').addEventListener('click', function(event){
 	
 	apiResp = JSON.parse(apiReq.responseText);
 	
+	 //display the temperature for the city we looked for.
 	document.getElementById('inputMethod').textContent = document.getElementById('weatherCity').value;
 	document.getElementById('temperature').textContent = apiResp.main.temp + " degrees C";
 	
+	//make sure that the button doesnt refresh the page
 	event.preventDefault();
 });
 
