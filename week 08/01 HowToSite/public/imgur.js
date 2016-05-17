0e74b8a6c8d0b1d952b2bdb9c715a4c7358d3c5c
knsy <ksy@yakovenko.net>
2016-05-16 23:00:17 -0700

    Added Random Cats Button
    Works pretty well, although sometimes it stalls kind of and you wonder
    what was going on, but it is actually loading a massive gif....

--- week 08/01 HowToSite/public/imgur.js
+++ week 08/01 HowToSite/public/imgur.js
@@ -3,7 +3,8 @@
 var clientID = "NULL";
 
 //this is where we will be sending the request. imgur's API
-var baseURL = "https://api.imgur.com/3/gallery/t/cats";
+//https://crossorigin.me/
+var baseURL = "https://api.imgur.com/3/gallery/search/?q=cats";
 
 //this means we are starting a new request that we will fill 
 //with all the necessary goodies.
@@ -12,29 +13,42 @@ var request = new XMLHttpRequest();
 //this starts filling in the details we put in earlier into our request.
 request.open('GET', baseURL, true);
 
+
 //here we put in our identification for imgur to recognize us
 request.setRequestHeader('Authorization', 'Client-ID ' + clientID);
 
 //here we check the response from imgur as soon as it gets back to us
 request.addEventListener("load", function() {
-  var response = JSON.parse(request.responseText);
+	loadImage();
+})
+
+request.send(null);
+
+document.getElementById('moreRandomImgur').addEventListener('click', loadImage);
+
+function loadImage(){
+	  var response = JSON.parse(request.responseText);
   
 	//here we put the response code up on the screen
-  document.getElementById('response').textContent = response.status;
+ // document.getElementById('response').textContent = response.status;
   console.log(response);
   
   //This gives us a number between 0 and 59.
   //since there are only 60 objects we get back.
   //we'll pick the random one of them.
-  var randomCat = Math.floor(Math.random()* 60);
+  var searchResponseSize = response.data.length;
+  do{
+	  
+		var randomCat = Math.floor(Math.random()* searchResponseSize);
+  } while(response.data[randomCat].is_album == true);
   
   //so, now lets go down that rabbit hole of objects and get the link
   //based on the random cat number we generated above.
-  var catLink = response.data.items[randomCat].link;
+  var catLink = response.data[randomCat].link;
   
   //display our link and make it point to the link address
-  document.getElementById('response').href = catLink;
-  document.getElementById('response').textContent = catLink;
-})
-
-request.send(null);
+  //document.getElementById('response').href = catLink;
+  //document.getElementById('response').textContent = catLink;
+  
+    document.getElementById('randomImage').src = catLink;
+};
