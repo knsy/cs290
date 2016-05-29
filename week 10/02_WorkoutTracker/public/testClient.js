be3de008d034a6240f90b4ec40a4dfedffa9f0ff
knsy <ksy@yakovenko.net>
2016-05-29 11:10:42 -0700

    Editing works.

--- week 10/02_WorkoutTracker/public/testClient.js
+++ week 10/02_WorkoutTracker/public/testClient.js
@@ -6,6 +6,7 @@ document.getElementById('postAdd').addEventListener('click', function(event){
 	payload.reps = document.getElementById('workoutReps').value;
 	payload.weight = document.getElementById('workoutWeight').value;
 	payload.date = document.getElementById('workoutDate').value;
+	payload.lbs = document.getElementById('workoutLbs').checked;
 	
 	req.open('POST', 'http://localhost:3000/insert', true);
 	req.setRequestHeader('Content-Type', 'application/json');
@@ -24,6 +25,44 @@ document.getElementById('postAdd').addEventListener('click', function(event){
 	//console.log("sent: " + payload);
 	event.preventDefault();
 });
+
+//////////////////////////EDIT SUBMIT/////////////////////////////
+document.getElementById('submitEditButton').addEventListener('click', function (event){
+		var req = new XMLHttpRequest();
+		var payload = {};
+		
+		payload.id = document.getElementById('workoutId').value;
+		payload.name = document.getElementById('workoutName').value;
+		payload.reps = document.getElementById('workoutReps').value;
+		payload.weight = document.getElementById('workoutWeight').value;
+		payload.date = document.getElementById('workoutDate').value;
+		payload.lbs = document.getElementById('workoutLbs').checked;
+		
+		req.open('POST', 'http://localhost:3000/update', true);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.addEventListener('load',function(){
+			 if(req.status >= 200 && req.status < 400){
+				 var response = JSON.parse(req.response);
+				 //console.log(response);
+				 
+				 //RETURN INPUT BOX TO NORMAL!
+				//hide Add btn, show SubmitEdit btn
+				document.getElementById('postAdd').hidden = false;
+				document.getElementById("submitEditButton").hidden = true;
+			 
+				 buildTable(response);
+				 
+		} else {
+			 console.log("Error in network request: " + req.statusText);
+		}
+		});
+		req.send(JSON.stringify(payload));
+		//console.log("sent: " + payload);
+		event.preventDefault();
+}
+);
+
+
 
 //////////////////////////DELETE//////////////////////////////////
 function deleteRow(taskNumber){
@@ -48,6 +87,7 @@ function deleteRow(taskNumber){
 	//console.log("sent: " + payload);
 	//event.preventDefault();
 };
+
 
 //on page load get table
 function onPageLoadGet(){
@@ -114,40 +154,60 @@ function buildTable(response){
 				
 				if(cell == 'id'){
 					newTableRow.name = response[row - 1][cell];
+				}else if(cell == 'date'){
+					//since the mysql date is a fucking atrocity
+					//that includes time, we trim the time out.
+					var newTableCell = document.createElement("td");
+					var dateLength = 10;
+					var fullDate = response[row - 1][cell];
+					
+					newTableCell.textContent = fullDate.substring(0, dateLength);
+					if(response[row - 1][cell] == null){
+						newTableCell.textContent = "NA";
+					}
+					newTableRow.appendChild(newTableCell);
 				}else{
 					var newTableCell = document.createElement("td");
 					newTableCell.textContent = response[row - 1][cell];
 					if(response[row - 1][cell] == null){
 						newTableCell.textContent = "NA";
 					}
-				newTableRow.appendChild(newTableCell);
+					newTableRow.appendChild(newTableCell);
 				}
 				
 			}
-				//add buttons
+				//add DELETE buttons
 				var deleteButton = document.createElement('input');
 				deleteButton.type = "button";
 				deleteButton.value = "Del";
 				deleteButton.className = "deleteButton"
 				newTableRow.appendChild(deleteButton);
+				
+				//add EDIT buttons
+				var editButton = document.createElement('input');
+				editButton.type = "button";
+				editButton.value = "Edit";
+				editButton.className = "editButton"
+				newTableRow.appendChild(editButton);
 		}
 		document.getElementById("workoutTable").appendChild(newTableRow);
 	}
 	
 	var deleteButtons = document.getElementsByClassName("deleteButton");
-	bindButtons(deleteButtons);
+	bindDeleteButtons(deleteButtons);
+	
+	var editButtons = document.getElementsByClassName("editButton");
+	bindEditButtons(editButtons);
 }
 
-function bindButtons(deleteButton){	
+function bindDeleteButtons(deleteButton){	
 	
 	for (var i = 0; i < deleteButton.length; i++) {
 		console.log("binding!!!");
 		deleteButton[i].addEventListener('click', function (event){
 		var req = new XMLHttpRequest();
 		var payload = {};
-		console.log("i: " + i);
-		console.log("dlt: " + deleteButton);
-		console.log("dlt[i]: " + deleteButton[i]);
+
 		payload.id = this.parentNode.name;
 		req.open('POST', 'http://localhost:3000/delete', true);
 		req.setRequestHeader('Content-Type', 'application/json');
@@ -169,4 +229,52 @@ function bindButtons(deleteButton){
 );
 }
 }
+
+function bindEditButtons(editButton){	
+	
+	for (var i = 0; i < editButton.length; i++) {
+		console.log("binding!!!");
+		editButton[i].addEventListener('click', function (event){
+			var req = new XMLHttpRequest();
+			var payload = {};
+
+			payload.id = this.parentNode.name;
+			req.open('POST', 'http://localhost:3000/getId', true);
+			req.setRequestHeader('Content-Type', 'application/json');
+			req.addEventListener('load',function(){
+				 if(req.status >= 200 && req.status < 400){
+					 var response = JSON.parse(req.response);
+					 //console.log(response);
+
+					document.getElementById('workoutId').value = response[0].id;
+					document.getElementById('workoutForm').textContent = "Edit Exercise:";
+					console.log(response);
+					document.getElementById('workoutName').value = response[0].name;
+					document.getElementById('workoutReps').value = response[0].reps;
+					document.getElementById('workoutWeight').value = response[0].weight;
+					document.getElementById('workoutLbs').checked = response[0].lbs;
+			
+					//since the mysql date is a fucking atrocity
+					//that includes time, we trim the time out.
+					var dateLength = 10;
+					var fullDate = response[0].date;
+					document.getElementById('workoutDate').value = fullDate.substring(0, dateLength);
+					
+					//hide Add btn, show SubmitEdit btn
+					document.getElementById('postAdd').hidden = true;
+					document.getElementById("submitEditButton").hidden = false;
+					
+		
+				} else {
+					 console.log("Error in network request: " + request.statusText);
+				}
+			});
+		req.send(JSON.stringify(payload));
+		//console.log("sent: " + payload);
+		event.preventDefault();
+		}
+		);
+	}
+}
+
 
--- week 10/02_WorkoutTracker/workoutTrackerServer.js
+++ week 10/02_WorkoutTracker/workoutTrackerServer.js
@@ -24,23 +24,16 @@ app.use(session({secret:'TheSecretThatShouldBeSecret'}));
 //processing the GET requests
 app.get('/', function(req,res){
 	var getContext = {};
-	
 	res.render('workoutPage', getContext);
 });
 
 app.post('/', function(req,res){
 	var getContext = {};
-	
 	res.render('workoutPage', getContext);
 })
 
 
-app.get('/testPost', function(req,res){
-	var getContext = {};
-	
-	res.render('testPage', getContext);
-});
-
+//returns the whole table
 app.get('/getTable', function(req,res,next){
 	var getContext = {};
 	  mysql.pool.query("SELECT * FROM workouts", function(err, rows, fields){
@@ -53,20 +46,27 @@ app.get('/getTable', function(req,res,next){
   });
 });
 	
-
-
-//
-app.get('/insert',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`) VALUES ('testName')", function(err, result){
+app.post('/getId',function(req,res,next){
+	var getContext = {};
+	if(req.body){
+		console.log(req.body);
+	}
+	//delete value
+	//var parsedQuery = JSON.parse(req.body);
+	//console.log(parsedQuery);
+	
+	mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.body.id], function(err, result){
     if(err){
       next(err);
       return;
     }
-    context.results = "Inserted id " + result.insertId;
-    //res.render('',context);
-	res.send(JSON.stringify(context));
+
+    getContext.table = JSON.stringify(result);
+	console.log(getContext.table);
+	res.send(getContext.table);
+	
   });
+
 });
 
 app.post('/delete',function(req,res,next){
@@ -83,8 +83,8 @@ app.post('/delete',function(req,res,next){
       next(err);
       return;
     }
-  });
-	//return new table
+	
+	// if deleted return new table
 	mysql.pool.query("SELECT * FROM workouts", function(err, rows, fields){
     if(err){
       next(err);
@@ -95,7 +95,10 @@ app.post('/delete',function(req,res,next){
 	console.log(getContext.table);
 	res.send(getContext.table);
 	
+	});
+	
   });
+
 });
 
 app.post('/insert', function(req,res,next){
@@ -106,25 +109,71 @@ app.post('/insert', function(req,res,next){
 	//add value
 	//var parsedQuery = JSON.parse(req.body);
 	//console.log(parsedQuery);
-	mysql.pool.query("INSERT INTO workouts SET name=?, reps=?, weight=?, date=?", [req.body.name, req.body.reps, req.body.weight, req.body.date], function(err, result){
+	mysql.pool.query("INSERT INTO workouts SET name=?, reps=?, weight=?, date=?, lbs=?", [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
     if(err){
       next(err);
       return;
     }
-  });
-	//return new table
+	
+	// if insert is good return new table
 	mysql.pool.query("SELECT * FROM workouts", function(err, rows, fields){
     if(err){
       next(err);
       return;
     }
-
-    getContext.table = JSON.stringify(rows);
+	
+	getContext.table = JSON.stringify(rows);
 	console.log(getContext.table);
 	res.send(getContext.table);
 	
-  });
+	});
+  });	
+});
+
+app.post('/update',function(req,res,next){
+	var getContext = {};
+	if(req.body){
+		console.log(req.body);
+	}
+	//delete value
+	//var parsedQuery = JSON.parse(req.body);
+	//console.log(parsedQuery);
 	
+	mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.body.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+
+	if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+        [req.body.name || curVals.name, req.body.reps || curVals.reps, req.body.weight || curVals.weight, req.body.date || curVals.date, req.body.lbs || curVals.lbs, req.body.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        
+		// if insert is good return new table
+		mysql.pool.query("SELECT * FROM workouts", function(err, rows, fields){
+		if(err){
+		  next(err);
+		  return;
+		}
+		
+		getContext.table = JSON.stringify(rows);
+		console.log(getContext.table);
+		res.send(getContext.table);
+		
+		});
+      });
+	
+	}
+    
+	
+  });
+
 });
 
 //RESET TABLE PAGE
@@ -141,7 +190,7 @@ app.get('/reset-table',function(req,res,next){
     mysql.pool.query(createString, function(err){
       
       console.log("Table reset");
-	  res.render('testPage', context);
+	  res.render('workoutPage', context);
 	  
     });
  });
