6137ec32abf843baa74ec3d52a49c9988aad487d
knsy <ksy@yakovenko.net>
2016-05-31 15:13:32 -0700

    Edit lbs true->false not working.

--- week 10/02_WorkoutTracker/workoutTrackerServer.js
+++ week 10/02_WorkoutTracker/workoutTrackerServer.js
@@ -148,7 +148,7 @@ app.post('/update',function(req,res,next){
 	if(result.length == 1){
       var curVals = result[0];
       mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-        [req.body.name || curVals.name, req.body.reps || curVals.reps, req.body.weight || curVals.weight, req.body.date || curVals.date, req.body.lbs || curVals.lbs, req.body.id],
+        [req.body.name || curVals.name, req.body.reps || curVals.reps, req.body.weight || curVals.weight, req.body.date || curVals.date, req.body.lbs, req.body.id],
         function(err, result){
         if(err){
           next(err);
